9dce895cfa8c933aa7a2ada8f86858a2bde884b9
Daniel <satelerd@gmail.com>
2022-09-12 14:24:26 -0300

    router link

--- frontend2/src/App.js
+++ frontend2/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Test from './pages/Test';
+
+
 
 function App() {
+  console.log("testing");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} excact />
+        <Route path='/test' element={<Test />} excact />
+      </Routes>
+    </Router>
   );
 }
 
