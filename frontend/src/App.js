337693d038e0a5f4c1c153dcabebff88218fe7ae
Daniel <satelerd@gmail.com>
2022-09-12 00:13:41 -0300

    react routering

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-
-
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
+import Test from './pages/Test';
 
 
 
@@ -35,7 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<Home />} excact />
-        {/* < */}
+        <Route path='/test' element={<Test />} excact />
       </Routes>
     </Router>
   );
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -9,6 +9,7 @@ function Home() {
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
+
                 <a
                     className="App-link"
                     href="https://reactjs.org"
