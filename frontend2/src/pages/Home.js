b1364483045ea6aa80dc8aa2891357a9c74e9487
Daniel <satelerd@gmail.com>
2022-09-14 20:02:42 -0300

    better divs

--- frontend2/src/pages/Home.js
+++ frontend2/src/pages/Home.js
@@ -15,30 +15,29 @@ function Home() {
                 <Navbar toggle={toggle} />
             </header>
             <div className="App-body">
-                {/* compoenente: landing */}
-                <h1>Prime 3D</h1>
-                <h2>Gana plata imprimiendo cuando tu quieras</h2>
-                <h2>Registrate</h2>
-                <h3>¿Como funciona?</h3>
-                <p>vNam suscipit euismod nunc tincidunt lacinia. Pellentesque hendrerit eu erat nec cursus. Vivamus dictum at risus eget vulputate. Sed porttitor mi urna, vitae auctor sem iaculis et. Ut sagittis, turpis id suscipit vestibulum, leo ante gravida libero, vel auctor dolor ante sed libero. Donec bibendum condimentum ex, in aliquam ex fermentum varius. Vestibulum dignissim tellus id vulputate dignissim. Maecenas rutrum odio ligula, quis mollis leo ultricies ut. Ut vel tortor erat. Curabitur scelerisque et justo a fringilla. Curabitur eget risus odio. Duis pulvinar volutpat scelerisque. Duis sit amet urna nunc. Sed sollicitudin sagittis tortor eget sagittis.</p>
+                <div className="landing">
+                    <h2>Registrate como Printer de</h2>
+                    <h1>Prime 3D</h1>
+                </div>
 
-                {/* componente: que buscamos */}
-                <h1>que buscamos</h1>
-                <h3>Nombre  y apellido</h3>
-                <input type='text'></input>
-                <h3>Correo electronico</h3>
-                <input type='text'></input>
-                <h3>Celular (+569)</h3>
-                <input type='text'></input>
-                <h3>Comuna</h3>
-                <input type='text'></input>
-                <input type='submit'></input>
+                <div className="what">
+                    <h3>¿Como funciona?</h3>
+                    <h2>Gana plata imprimiendo cuando tu quieras</h2>
+                    <p>vNam suscipit euismod nunc tincidunt lacinia. Pellentesque hendrerit eu erat nec cursus. Vivamus dictum at risus eget vulputate. Sed porttitor mi urna, vitae auctor sem iaculis et. Ut sagittis, turpis id suscipit vestibulum, leo ante gravida libero, vel auctor dolor ante sed libero. Donec bibendum condimentum ex, in aliquam ex fermentum varius. Vestibulum dignissim tellus id vulputate dignissim. Maecenas rutrum odio ligula, quis mollis leo ultricies ut. Ut vel tortor erat. Curabitur scelerisque et justo a fringilla. Curabitur eget risus odio. Duis pulvinar volutpat scelerisque. Duis sit amet urna nunc. Sed sollicitudin sagittis tortor eget sagittis.</p>
+                </div>
 
-
-                {/* 
-                    <Componente: landing />
-                    <Componente: que buscamos />
-                */}
+                <div className='register'>
+                    <h2>Registrate</h2>
+                    <h3>Nombre  y apellido</h3>
+                    <input type='text'></input>
+                    <h3>Correo electronico</h3>
+                    <input type='text'></input>
+                    <h3>Celular (+569)</h3>
+                    <input type='text'></input>
+                    <h3>Comuna</h3>
+                    <input type='text'></input>
+                    <input type='submit'></input>
+                </div>
             </div>
         </div>
     );
