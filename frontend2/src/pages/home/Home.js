25579c9953686e31eea56e51102cd9b40b4f8c01
agustindiazad <agudiaaz1@gmail.com>
2022-11-12 18:26:21 -0300

    box why

--- frontend2/src/pages/home/Home.js
+++ frontend2/src/pages/home/Home.js
@@ -21,7 +21,9 @@ function Home() {
                 </div>
 
                 <div className="us">
-                    <h2>Gana plata imprimiendo cuando tu quieras</h2>
+                    <h2>Gana plata a traves de la impresion 3D</h2>
+                </div>
+                <div>
                     {/* a youtube video */}
                     <iframe className='video' width="560" height="315" src="https://www.youtube.com/embed/_uEUMZvTH7E?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     {/* <iframe className='video' width="560" height="315" src="https://www.youtube.com/embed/2Z4m4lnjxkY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
@@ -30,7 +32,8 @@ function Home() {
                 <div className="why">
                     <div className="why-text">
                         <h2>¿Por que Prime 3D?</h2>
-                        <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies lacinia, nisl nisl aliquet nisl, nec aliquam nisl nisl vel nisl. Donec auctor, nisl eget ultricies lacinia, nisl nisl aliquet nisl, nec aliquam nisl nisl vel nisl.</h4>
+                        <h4>La impresión 3d es una herramienta fundamental hoy en día, permite generar objetos útiles para el día a día e incluso herramientas que pueden ser utilizadas para grandes proyectos.</h4>
+                        <h4>Prime 3d es ideal para aquellos/as que no poseen impresoras 3d, ya sea por sus precios o por su complicado uso. Prime busca facilitar el acceso de todas las personas aquellas estén interesadas en imprimir estos objetos y que estén dispuestos a adentrarse en este mundo.</h4>
                     </div>
 
                     <div className='register'>
