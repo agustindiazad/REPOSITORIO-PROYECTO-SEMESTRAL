2970b85837fcb1eb9c2e5ddff4ce1086d44911b7
agustindiazad <agudiaaz1@gmail.com>
2022-11-22 12:03:50 -0300

    option-funcion

--- frontend2/src/pages/home/Home.js
+++ frontend2/src/pages/home/Home.js
@@ -23,40 +23,40 @@ function Home() {
                 <div className="us">
                     <h2>Gana plata a traves de la impresion 3D</h2>
                 </div>
-                <div>
-                    {/* a youtube video */}
-                    <iframe className='video' width="560" height="315" src="https://www.youtube.com/embed/_uEUMZvTH7E?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    {/* <iframe className='video' width="560" height="315" src="https://www.youtube.com/embed/2Z4m4lnjxkY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
-                </div>
-
                 <div className="why">
                     <div className="why-text">
                         <h2>¿Por que Prime 3D?</h2>
                         <h4>La impresión 3d es una herramienta fundamental hoy en día, permite generar objetos útiles para el día a día e incluso herramientas que pueden ser utilizadas para grandes proyectos.</h4>
                         <h4>Prime 3d es ideal para aquellos/as que no poseen impresoras 3d, ya sea por sus precios o por su complicado uso. Prime busca facilitar el acceso de todas las personas aquellas estén interesadas en imprimir estos objetos y que estén dispuestos a adentrarse en este mundo.</h4>
                     </div>
-
-                    <div className='register'>
-                        <h2 className='input-h2'>Registrate</h2>
-                        <div className='inputs'>
-                            <div className='input'>
-                                <h3 className='input-h3'>Nombre  y apellido</h3>
-                                <input type='text'></input>
-                            </div>
-                            <div className='input'>
-                                <h3 className='input-h3'>Correo electronico</h3>
-                                <input type='text'></input>
-                            </div>
-                            <div className='input'>
-                                <h3 className='input-h3'>Celular (+569)</h3>
-                                <input type='text'></input>
-                            </div>
-                            <div className='input'>
-                                <h3 className='input-h3'>Comuna</h3>
-                                <input type='text'></input>
-                            </div>
-                            <input type='submit'></input>
+                    <div className='why-option'>
+                        <h2>¿Somos una buena opción?</h2>
+                        <h4>jdfnvjkdvdjvjdfv djfv dfv kdfvndjkfv  dfkvjndfkvjndfjn</h4>
+                    </div>
+                    <div className='why-funcion'>
+                        <h2>¿Como funciona Prime3D</h2>
+                    </div>
+                </div>
+                <div className='register'>
+                    <h2 className='input-h2'>Registrate</h2>
+                    <div className='inputs'>
+                        <div className='input'>
+                            <h3 className='input-h3'>Nombre  y apellido</h3>
+                            <input type='text'></input>
                         </div>
+                        <div className='input'>
+                            <h3 className='input-h3'>Correo electronico</h3>
+                            <input type='text'></input>
+                        </div>
+                        <div className='input'>
+                            <h3 className='input-h3'>Celular (+569)</h3>
+                            <input type='text'></input>
+                        </div>
+                        <div className='input'>
+                            <h3 className='input-h3'>Comuna</h3>
+                            <input type='text'></input>
+                        </div>
+                        <input type='submit'></input>
                     </div>
                 </div>
             </div>
