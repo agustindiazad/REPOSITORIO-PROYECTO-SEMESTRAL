4dad79af1bab804a7d24aab79ee4b6efe10c4a65
agustindiazad <agudiaaz1@gmail.com>
2022-11-22 23:03:21 -0300

    why-us

--- frontend2/src/pages/home/Home.js
+++ frontend2/src/pages/home/Home.js
@@ -18,26 +18,26 @@ function Home() {
                 <div className="landing">
                     <h2 className='subtitle'>Unete a la comunidad de Printers en</h2>
                     <h1 className='title'>Prime 3D</h1>
+                    
                 </div>
 
                 <div className="win">
                     <h2>Gana plata a traves de la impresion 3D</h2>
-                    <h3>uhdjhfsdjhfshjdfhjsdf</h3>
                 </div>
-                <div className='margen'>
-                    <div className="why">
-                        <div className="why-text">
+            </div>
+            <div className='margen'>
+                <div className="why">
+                    <div className='why-col'>
+                        <div className="why-title">
                             <h2>¿Por que Prime 3D?</h2>
+                        </div>
+                        <div className='why-text'>
                             <h4>La impresión 3d es una herramienta fundamental hoy en día, permite generar objetos útiles para el día a día e incluso herramientas que pueden ser utilizadas para grandes proyectos.</h4>
                             <h4>Prime 3d es ideal para aquellos/as que no poseen impresoras 3d, ya sea por sus precios o por su complicado uso. Prime busca facilitar el acceso de todas las personas aquellas estén interesadas en imprimir estos objetos y que estén dispuestos a adentrarse en este mundo.</h4>
-                            <div className='img-text'>
-                                <img src='https://cdn-icons-png.flaticon.com/512/1909/1909783.png'></img>
-                            </div>
                         </div>
-                        <div className='why-option'>
-                            <h2>¿Sómos buena opción?</h2>
-                            <h4>Si debido a jjjjjddkdl kkskdjnf ksfkjsnf fkkkfk ff f</h4>
-                        </div>
+                    </div>
+                    <div className='img-text'>
+                        <img src='https://cdn-icons-png.flaticon.com/512/1909/1909783.png'></img>
                     </div>
                 </div>
             </div>
